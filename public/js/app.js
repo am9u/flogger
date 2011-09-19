bd1c1364e1610a9ecf3de5c0f6e8df72d20c2b60
Jon Ferrer <jon@jonferrer.com>
2011-09-18 20:39:45 -0400

    set up markup, templates and backbone code for add exercise client-side application

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,9 @@
-
 (function (window, document, $, _, Backbone) {
 
 _.templateSettings = {
    interpolate : /\{\{(.+?)\}\}/g
  };
     
-
 window.ExerciseType = Backbone.Model.extend({});
 
 window.ExerciseTypes = Backbone.Collection.extend({
@@ -26,7 +24,7 @@ window.ExerciseTypeRow = Backbone.View.extend({
     },
 
     render : function () {
-        console.log(this.model.get("name"));
+        //console.log(this.model.get("name"));
         /*
         $(this.el).html(this.template({
             label : this.model.get("name"),
@@ -65,68 +63,126 @@ window.ExerciseTypeList = Backbone.View.extend({
     }
 });
 
+window.ExerciseSetRow = Backbone.View.extend({
+
+    tagName : "table",
+    className : "table-exercise-sets",
+
+    initialize : function () {
+        if ( ! this.template) {
+            this.template = $("#tpl-exercise-set").html();
+        }
+    },
+
+    render : function () {
+        $(this.el).append(this.template); 
+        return this;
+    }
+
+});
+
 window.ExerciseRow = Backbone.View.extend({
 
     tagName : "div",
 
+    events : {
+        "click .btn-add-exercise-set" : "addExerciseSet"
+    },
+
     initialize : function () {
         _.bindAll(this, "render");
+
+        if ( ! this.template) {
+          this.template = $("#tpl-add-exercise").html();
+        } 
     },
 
     render : function () {
-        var exerciseTypeList = new ExerciseTypeList().render(Exercises).el,
-            exerciseCell = $("<div />", { "class" : "col-exercise-list" }).append(exerciseTypeList),
-            
-            targetMinRepsCell = $("<div />", { "class" : "col-target-reps" }),
-            targetMinRepsField = $("<input />", { 
-                "class" : "field-reps",
-                name : "target_min_reps"
-            }).appendTo(targetMinRepsCell),
 
-            targetMaxRepsCell = $("<div />", { "class" : "col-target-reps" }),
-            targetMaxRepsField = $("<input />", { 
-                "class" : "field-reps",
-                name : "target_max_reps"
-            }).appendTo(targetMaxRepsCell),
+        var el = $(this.el).html(this.template);
 
-            actualMinRepsCell = $("<div />", { "class" : "col-actual-reps" }),
-            actualMinRepsField = $("<input />", { 
-                "class" : "field-reps",
-                name : "actual_min_reps"
-            }).appendTo(actualMinRepsCell),
+        el.find(".col-exercise-list")
+            .append(new ExerciseTypeList().render(Exercises).el);
 
-            actualMaxRepsCell = $("<div />", { "class" : "col-actual-reps" }),
-            actualMaxRepsField = $("<input />", { 
-                "class" : "field-reps",
-                name : "actual_max_reps"
-            }).appendTo(actualMaxRepsCell);
-
-
-        $(this.el)
-            .append(exerciseCell)
-            .append(targetMinRepsCell)
-            .append(targetMaxRepsCell)
-            .append(actualMinRepsCell)
-            .append(actualMaxRepsCell);
-
-        //$(exerciseTypeList).chosen();
+        this.addExerciseSet();
 
         return this;
+    },
+
+    addExerciseSet : function () {
+        $(this.el).find(".exercise-sets-container")
+            .append(new ExerciseSetRow().render().el);
     }
 });
 
-$("#btn-add-exercise").live("click", function (event) {
-    $("#exercises").append(new ExerciseRow().render().el);
+window.AddExerciseView = Backbone.View.extend({
+    tagName : "div",
+    className : "page-view hide",
+
+    events : {
+        "click .btn-cancel" : "hide"
+    },
+
+    initialize : function () {
+        if ( ! this.template) {
+            this.template = $("#tpl-page-add-exercise").html();
+        }
+    },
+
+    render : function (container) {
+        $(this.el)
+            .html(this.template)
+            .find(".panel").append(new ExerciseRow().render().el);
+
+        container.append(this.el);
+
+        this.trigger("render");
+        return this;
+    },
+
+    show : function () {
+        console.log("show called");
+        $(this.el)
+            .removeClass("hide");
+    },
+
+    hide : function () {
+        $(this.el)
+            .addClass("hide");
+
+        var onHideEnd = _.bind(function (event) {
+            if (event.propertyName === "top") {
+                this.el.removeEventListener("webkitTransitionEnd", onHideEnd);
+                $(this.el).remove();
+            }
+        }, this);
+
+        this.el.addEventListener("webkitTransitionEnd", onHideEnd);
+    }
+});
+
+window.AppView = Backbone.View.extend({
+    events : {
+        "click #btn-add-exercise" : "displayAddExercise"
+    },
+
+    initialize : function () {},
+
+    displayAddExercise : function () {
+        var addExercise = new AddExerciseView();
+        addExercise.bind("render", function () {
+            var show = _.bind(this.show, this);
+            _.delay(show, 10);
+        }, addExercise);
+        addExercise.render($("#app-view"));
+    }
 });
 
 $(function () {
-    $("#exercises").append(new ExerciseRow().render().el);
-});
-
-//ExerciseTypes.fetch();
-
-window.AddExerciseView = Backbone.View.extend({
-
+    var app = new AppView({
+        el : "#app-view"    
+    });
+    app.render();
 });
 
 }(this, this.document, this.jQuery, this._, this.Backbone));
